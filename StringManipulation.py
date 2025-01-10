92f39cc8a1c41c57e58956a1f2f695ae45cc9f45
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-01-10 10:35:04 +0530

    Add files via upload

---
+++ StringManipulation.py
@@ -0,0 +1,7 @@
+#STRINT MANIPULATION
+
+# Use \n to add another line of "Hello world".
+print("hello world.\nhello shub.\nhello dev.")
+
+# Add a space between the strings
+print("hello" + " " + "angela!")
